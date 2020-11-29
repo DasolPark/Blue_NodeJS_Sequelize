271de8e7a0f1de1727df4251a2cefd4d356c27c7
Dasol Park <dasol.park615@gmail.com>
2020-11-29 20:16:54 +0900

    add sequelize and body-parser

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Routes
 app.get('/', (req, res) => res.send('INDEX'));
 
 const PORT = process.env.PORT || 3000;
