fe31ce88d8f315f4d1ab5cc43e47ebc8ae643de4
Dasol Park <dasol.park615@gmail.com>
2020-11-30 21:31:21 +0900

    add validation

--- routes/notices.js
+++ routes/notices.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const Notice = require('../models/Notice')
+const Notice = require('../models/Notice');
 
 // Index
 router.get('/', async (req, res) => {
   try {
     const notices = await Notice.findAll();
-    res.render('notices', { notices: notices.map(notice => notice.toJSON()) })
+    res.render('notices', { notices: notices.map(notice => notice.toJSON()) });
   } catch (error) {
     console.log(error);
   }
@@ -16,22 +16,35 @@ router.get('/', async (req, res) => {
 router.get('/add', (req, res) => res.render('add'));
 
 router.post('/add', async (req, res) => {
-  // const data = {
-  //   title: 'fake title',
-  //   content: 'fake content',
-  // }
+  let { title, content } = req.body;
+  const errors = [];
 
-  // const { title, content } = data;
-  const { title, content } = req.body;
-
-  try {
-    await Notice.create({
-      title, content
-    })
-    res.redirect('/notices');
-  } catch (error) {
-    console.log(error);
+  // Validate Fields
+  if (!title) {
+    errors.push({ text: 'Please add a title' });
   }
-})
+  if (!content) {
+    errors.push({ text: 'please add a content' });
+  }
+
+  if (errors.length > 0) {
+    res.render('add', {
+      errors,
+      title,
+      content
+    })
+  } else {
+    title = title.toUpperCase().trim();
+
+    try {
+      await Notice.create({
+        title, content
+      })
+      res.redirect('/notices');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
 
 module.exports = router;
